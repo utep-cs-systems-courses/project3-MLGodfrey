973472d599c0a11f1cd2f4751f2c951df717ed0a
MLGodfrey <85763950+MLGodfrey@users.noreply.github.com>
2021-07-26 18:00:53 -0600

    Delete p1_interrupt_handler.c

--- wakedemo/p1_interrupt_handler.c
+++
@@ -1,9 +0,0 @@
-#include <msp430.h>
-#include "switches.h"
-
-void __interrupt_vec(PORT2_VECTOR) Port_2(){
-  if (P2IFG & SWITCHES) { //is a button causing an interrupt
-    P2IFG &= ~SWITCHES;   //clears pending interrupts   
-    switch_interrupt_handler();//handles all switches
-  }
-}
