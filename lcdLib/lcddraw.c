fe563bcc5bfc3ca2a118ab5956669b62a29135ed
MLGodfrey <85763950+MLGodfrey@users.noreply.github.com>
2021-07-27 17:50:51 -0600

    Update lcddraw.c

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -5,6 +5,54 @@
 #include "lcddraw.h"
 
 
+int offsetX = 25;
+int offsetY = 50;
+
+
+void move_shape_Left(int x){
+  my_shape(COLOR_WHITE);
+    offsetX-=x;
+}
+void move_shape_Right(int x){
+  my_shape(COLOR_WHITE);
+  offsetX+=x;
+}
+void move_shape_Up(int y){
+  my_shape(COLOR_WHITE);
+  offsetY-=y; 
+}
+void move_shape_Down(int y){
+  my_shape(COLOR_WHITE);
+  offsetY+=y; 
+}
+
+void my_shape(u_int color){
+  fillRectangle(offsetX+1, offsetY+15, 50, 20, color);
+  fillRectangle(offsetX+15, offsetY+1, 20, 50, color);
+
+  // Left Arrow
+
+  int offsetXTL= offsetX;     // X Triangle Left
+  int offsetYTL= offsetY+49; // Y Triangle Left
+  
+  // Right Arrow
+  int offsetXTR= offsetX+50;   // X Triangle Right
+  int offsetYTR= offsetY; // Y Triangle Right
+
+  for(int r=0; r<25; r++){
+    for(int c=0; c<=r; c++){
+      
+      // Left Arrow
+      drawPixel(offsetX-c, offsetY+r, color);
+      drawPixel(offsetXTL-c, offsetYTL-r, color);
+
+      // Right Arrow
+      drawPixel(offsetXTR+c, offsetYTL-r, color);
+      drawPixel(offsetXTR+c, offsetYTR+r, color);
+    }
+  }
+}
+
 /** Draw single pixel at x,row 
  *
  *  \param col Column to draw to
