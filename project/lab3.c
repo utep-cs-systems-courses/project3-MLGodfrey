ed3a50d3dc22ec90118fdbaf99619b93076befd1
MLGodfrey <85763950+MLGodfrey@users.noreply.github.com>
2021-07-28 18:26:58 -0600

    Update lab3.c

--- project/lab3.c
+++ project/lab3.c
@@ -3,56 +3,56 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 #include "buzzer.h"
-#include "switches.h"
-#include "led.h"
+//#include "switches.h"
+//#include "led.h"
 
 // WARNING: LCD DISPLAY USES P1.0.  Do not touch!!! 
 
-//#define LED BIT6		/* note that bit zero req'd for display */
+#define LED BIT6		/* note that bit zero req'd for display */
 
-/*#define SW1 1
+#define SW1 1
 #define SW2 2
 #define SW3 4
 #define SW4 8
 
 #define SWITCHES 15*/
 
-//static char 
-//switch_update_interrupt_sense()
-//{
-//  char p2val = P2IN;
-  /* update switch interrupt to detect changes from current buttons */
-//  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
-//  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
-//  return p2val;
-//}
+static char 
+switch_update_interrupt_sense()
+{
+  char p2val = P2IN;
+   /*update switch interrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
+}
 
-//void 
-//switch_init()			/* setup switch */
-//{  
-//  P2REN |= SWITCHES;		/* enables resistors for switches */
-//  P2IE |= SWITCHES;		/* enable interrupts from switches */
-//  P2OUT |= SWITCHES;		/* pull-ups for switches */
-//  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
-//  switch_update_interrupt_sense();
-//}
+void 
+switch_init()			/* setup switch */
+{  
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE |= SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
+  switch_update_interrupt_sense();
+  }
 
-//int switches = 0;
+int switches = 0;
 
-//void
-//switch_interrupt_handler()
-//{
-  // char p2val = switch_update_interrupt_sense();
-  // switches = ~p2val & SWITCHES;
-  //}
+void
+switch_interrupt_handler()
+{
+   char p2val = switch_update_interrupt_sense();
+   switches = ~p2val & SWITCHES;
+  }
 
 
-// axis zero for col, axis 1 for row
-//short drawPos[2] = {10,10}, controlPos[2] = {10,10};
-//short velocity[2] = {3,8}, limits[2] = {screenWidth-36, screenHeight-8};
+ axis zero for col, axis 1 for row
+short drawPos[2] = {10,10}, controlPos[2] = {10,10};
+short velocity[2] = {3,8}, limits[2] = {screenWidth-36, screenHeight-8};
 
 short redrawScreen = 1;
-//u_int controlFontColor = COLOR_GREEN;
+u_int controlFontColor = COLOR_GREEN;
 
 void wdt_c_handler() //controls the speed at which the image is drawn
 {
@@ -70,8 +70,8 @@ void update_shape();
 void main()
 {
   
-//P1DIR |= LED;		/**< Green led on when CPU on */
-//P1OUT |= LED;
+P1DIR |= LED;		/**< Green led on when CPU on */
+P1OUT |= LED;
   configureClocks();
   lcd_init();
   switch_init();
@@ -81,22 +81,22 @@ void main()
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
   
-  //clearScreen(COLOR_GREEN);
+  clearScreen(COLOR_GREEN);
   while (1) {			/* forever */
     if (redrawScreen) {
       redrawScreen = 0;
-      //update_shape();
-      my_shape(COLOR_RED);
+      update_shape();
+      //my_shape(COLOR_RED);
     }
-// P1OUT &= ~LED;	/* led off */
-//  or_sr(0x10);	/**< CPU OFF */
-//  P1OUT |= LED;	/* led on */
+ P1OUT &= ~LED;	/* led off */
+  or_sr(0x10);	/**< CPU OFF */
+  P1OUT |= LED;	/* led on */
   }
 }
 
     
     
-/*void
+void
 update_shape()
 {
   static unsigned char row = screenHeight / 2, col = screenWidth / 2;
@@ -119,13 +119,13 @@ update_shape()
      clearScreen(COLOR_GREEN);
      step = 0;
   }
-  }*/
+  }
 
 /* Switch on S2 */
-//void
-//__interrupt_vec(PORT2_VECTOR) Port_2(){
-//if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
-//  P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
-//  switch_interrupt_handler();	/* single handler for all switches */
-// }
-//}
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+  P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+  switch_interrupt_handler();	/* single handler for all switches */
+ }
+}
